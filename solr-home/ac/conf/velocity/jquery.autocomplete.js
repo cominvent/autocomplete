cdb60db94758a89b91be481b64452f119acfa538
Jan Høydahl <jan.git@cominvent.com>
2013-01-16 16:23:57 +0100

    Further improve autocomplete
    Also see SOLR-4309

--- solr-home/ac/conf/velocity/jquery.autocomplete.js
+++ solr-home/ac/conf/velocity/jquery.autocomplete.js
@@ -438,7 +438,7 @@ $.Autocompleter.defaults = {
 	max: 1000,
 	mustMatch: false,
 	extraParams: {},
-	selectFirst: true,
+	selectFirst: false,
 	formatItem: function(row) { return row[0]; },
 	formatMatch: null,
 	autoFill: false,
@@ -706,8 +706,8 @@ $.Autocompleter.Select = function (options, input, select, config) {
 		}
 		listItems = list.find("li");
 		if ( options.selectFirst ) {
-			// listItems.slice(0, 1).addClass(CLASSES.ACTIVE);
-			// active = 0;
+			listItems.slice(0, 1).addClass(CLASSES.ACTIVE);
+			active = 0;
 		}
 		// apply bgiframe if available
 		if ( $.fn.bgiframe )
